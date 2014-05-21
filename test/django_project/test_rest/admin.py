64b2ec84a1877db0f3026e0bb3158db70487c884
JustinTArthur <justinarthur@gmail.com>
2014-05-21 10:29:15 -0400

    Admin additions to make it easier.
    For blacklocus/angular-django-rest-resource#5.

--- test/django_project/test_rest/admin.py
+++ test/django_project/test_rest/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from test_rest.models import Plant, Animal
+
 # Register your models here.
+admin.site.register(Plant)
+admin.site.register(Animal)
--- test/django_project/test_rest/models.py
+++ test/django_project/test_rest/models.py
@@ -8,9 +8,16 @@ class Plant(models.Model):
     is_succulent = models.BooleanField()
     is_evergreen = models.BooleanField()
 
+    def __unicode__(self):
+        return self.common_name
+
+
 class Animal(models.Model):
     common_name = models.CharField(max_length=512)
     scientific_name = models.CharField(max_length=1024)
     is_mammal = models.BooleanField()
     is_reptile = models.BooleanField()
     is_arachnid = models.BooleanField()
+
+    def __unicode__(self):
+        return self.common_name
