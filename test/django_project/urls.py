597fb833ba4d05dc4d17f3e137bf64628069c287
JustinTArthur <justinarthur@gmail.com>
2014-06-04 12:21:31 -0500

    Serve up the library from the main source directory. Fixes to page to make it function.
    For blacklocus/angular-django-rest-resource#5.

--- test/django_project/urls.py
+++ test/django_project/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 from rest_framework import routers
 from test_rest import views
+from os import path
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,6 +13,10 @@ rest_router.register(r'animals', views.AnimalViewSet)
 rest_router.register(r'plants', views.PlantViewSet)
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url="/static/index.html")),
+    url(r'^(?P<path>angular-django-rest-resource\.js)$', 'django.views.static.serve', {
+        'document_root': path.join(settings.BASE_DIR, ".."),
+    }),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(rest_router.urls)),
 )
